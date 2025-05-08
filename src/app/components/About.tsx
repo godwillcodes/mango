c6ec647cd087a022bf147d5ebddf3fd858679329
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2025-05-08 18:46:16 +0300

    feat: add About component with initial structure and mobile menu state

---
+++ src/app/components/About.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+export default function Hero() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-white">
+        
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/Hero";
 import Header from "./components/Header";
+import AboutSection from "./components/About";
 
 export default function Home() {
   return (
     <div className="">
       <Header/>
       <HeroSection/>
+      <AboutSection/>
      
     </div>
   );
